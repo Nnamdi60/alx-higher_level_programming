d83d8241218ef00e8ca25487f754b09f057bb601
Nnamdi60 <christiannnamdi60@gmail.com>
2022-11-29 19:57:10 -0800

    The brain is a tool for invention

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
-
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-            print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+for i in range(10):
+    for j in range(10):
+        if j != i:
+            if i < j:
+                if i == 8 and j == 9:
+                    print("{}{}".format(i, j))
+                else:
+                    print("{}{}".format(i, j), end=", ")
