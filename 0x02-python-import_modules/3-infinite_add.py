407648399b60f6223170a7258a768c60d32ac90a
Nnamdi60 <christiannnamdi60@gmail.com>
2022-12-01 16:57:22 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-import string
-print(string.ascii_uppercase)
+
+from sys import argv
+
+if __name__ == "__main__":
+    arg_num = len(argv) - 1
+    if arg_num == 0:
+        print("{}".format(arg_num))
+    else:
+        result = []
+        for i in range(1, arg_num + 1):
+            result.append(int(argv[i]))
+        print("{}".format(sum(result)))
