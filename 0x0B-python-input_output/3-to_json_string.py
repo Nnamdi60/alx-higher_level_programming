d3389a10a955158c3f30b1fad6bb42b8a37aa95d
Nnamdi60 <christiannnamdi60@gmail.com>
2023-01-10 20:03:52 -0800

    The Jason string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""
-contain the JSON string
-"""
+''' function that returns the JSON representation of an object (string)
+'''
 
 import json
 
 
 def to_json_string(my_obj):
-    """returns the JSON representation of an object (string)"""
+    ''' module to_json_strin
+     returns JSON representation
+    '''
     return json.dumps(my_obj)
